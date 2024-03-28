a0012ce869a124128b53a7fe4f38e8fd5335870f
Rich Tebb <rich@tebb.io>
2024-03-28 19:27:44 +0000

    Use API token

--- tools/get_logs.py
+++ tools/get_logs.py
@@ -8,17 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
 
-abs_cookie_jar = aiohttp.CookieJar()
-
-
-async def is_logged_in(session: aiohttp.ClientSession):
-    url = "https://admin.bwccstats.com/api/is_logged_in"
-    async with session.get(url) as resp:
-        r = await resp.json()
-        print(r)
+headers = {"Authorization": f"Bearer {API_KEY}"}
 
 
 async def get_logs(session: aiohttp.ClientSession, from_):
@@ -29,26 +20,14 @@ async def get_logs(session: aiohttp.ClientSession, from_):
         "time_sort": "asc",
         "output": "json",
     }
-    resp = await session.post(url=url, json=body)
+    resp = await session.post(url=url, json=body, headers=headers)
     r = await resp.json()
-    print(r)
-
-
-async def login(session: aiohttp.ClientSession):
-    url = "https://admin.bwccstats.com/api/login"
-    body = {"username": USERNAME, "password": PASSWORD}
-    resp = await session.post(url=url, json=body)
-    r = await resp.text()
     print(r)
 
 
 async def main():
     conn = aiohttp.TCPConnector(limit_per_host=100, limit=0, ttl_dns_cache=300)
-    async with aiohttp.ClientSession(
-        connector=conn, cookie_jar=abs_cookie_jar
-    ) as session:
-        await login(session)
-        await is_logged_in(session)
+    async with aiohttp.ClientSession(connector=conn) as session:
         await get_logs(session, "")
 
 
